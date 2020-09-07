ce4f1390e4dbb4140cf3d72916ee0ff1a1d7f339
Dan Faulkner <danfaulkner@hollandandbarrett.com>
2020-09-07 20:14:34 +0100

    feat: create studioGhibliAPI

--- src/App.js
+++ src/App.js
@@ -1,48 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import List from "./components/list";
 import Search from "./components/search";
-import './App.css';
+import "./App.css";
 
 const App = () => {
-
   const stories = [
     {
-      title: 'React',
-      url: 'https://react.js.org/',
-      author: 'Jordan Walke',
+      title: "React",
+      url: "https://react.js.org/",
+      author: "Jordan Walke",
       num_comments: 3,
       points: 4,
-      objectID: 0
+      objectID: 0,
     },
     {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Amabrov, Andrew Clark',
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Amabrov, Andrew Clark",
       num_comments: 2,
       points: 5,
-      objectID: 1
-    }
-  ]
+      objectID: 1,
+    },
+  ];
 
-  const [ searchTerm, setSearchTerm ] = useState('')
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log("no data", err);
+      });
+  }, []);
+
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleChange = (event) => {
-    setSearchTerm(event.target.value)
-    console.log(searchTerm)
-  } 
+    setSearchTerm(event.target.value);
+    console.log(searchTerm);
+  };
 
-  const searchedStories = stories.filter(story => 
+  const searchedStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  )
+  );
 
   return (
     <div className="container">
       <h1>My Hacker Stories</h1>
-      <Search onSearch={handleChange} />   
-      <hr/>
+      <Search onSearch={handleChange} />
+      <hr />
       <List list={searchedStories} />
+      {data.title.map((item) => (
+        <li>item</li>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
